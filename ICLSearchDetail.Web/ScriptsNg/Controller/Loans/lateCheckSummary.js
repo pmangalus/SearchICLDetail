5b7b87b609e19b407281441615e3873086c4de48
Levi Santos <lsantos2@securitybank.com.ph>
2019-10-23 11:52:23 +0800

    nicole-add view for loans
    nicole-add view for loans

---
+++ ICLSearchDetail.Web/ScriptsNg/Controller/Loans/lateCheckSummary.js
@@ -0,0 +1,28 @@
+﻿var app = angular.module("lateCheckSummary", []);
+app.controller('lateCheckSummary', function ($scope) {
+
+    console.log("#1 lateCheckSummary controller JS");
+    var host = 'http://10.200.1.39:9861';
+    //var host = 'http://localhost:53325';
+
+    var apiUrl = host + '/api/cics/LLateCheckSummary/';
+    var that = this;
+    //This will hide the DIV by default.
+    $scope.IsVisible = false;
+    $scope.ShowSearch = function (value) {
+        //If DIV is visible it will be hidden and vice versa.
+
+        $scope.IsVisible = value == "Y";
+    };
+    $scope.btnGoModal = function () {
+        var result
+        if (document.getElementById('radioSearchBatch').checked) {
+
+            result = document.getElementById('selectResult').value;
+
+
+        }
+
+    }
+
+});
