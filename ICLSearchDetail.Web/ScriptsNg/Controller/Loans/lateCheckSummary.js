4a7b783bbca44190b5136c5bdca92a04361bb458
Levi Santos <lsantos2@securitybank.com.ph>
2019-10-24 09:45:20 +0800

    changes ui
    changes ui

--- ICLSearchDetail.Web/ScriptsNg/Controller/Loans/lateCheckSummary.js
+++ ICLSearchDetail.Web/ScriptsNg/Controller/Loans/lateCheckSummary.js
@@ -1,5 +1,6 @@
-﻿var app = angular.module("lateCheckSummary", []);
-app.controller('lateCheckSummary', function ($scope) {
+﻿
+app.controller('lateCheckSummary', ['$scope', '$timeout', function ($scope, $timeout) {
+
 
     console.log("#1 lateCheckSummary controller JS");
     var host = 'http://10.200.1.39:9861';
@@ -8,21 +9,16 @@ app.controller('lateCheckSummary', function ($scope) {
     var apiUrl = host + '/api/cics/LLateCheckSummary/';
     var that = this;
     //This will hide the DIV by default.
-    $scope.IsVisible = false;
-    $scope.ShowSearch = function (value) {
-        //If DIV is visible it will be hidden and vice versa.
-
-        $scope.IsVisible = value == "Y";
-    };
     $scope.btnGoModal = function () {
+        console.log('nakapasok na');
         var result
         if (document.getElementById('radioSearchBatch').checked) {
 
             result = document.getElementById('selectResult').value;
-
+            
 
         }
 
     }
 
-});
+}]);
